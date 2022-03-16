1a6d633bab1bee3a2f6ddaf1cbcfc6db2e795a10
nankuF <fireloki87@gmail.com>
2022-03-16 22:30:52 +0300

    rename files

--- send_email.py
+++ send_email.py
@@ -1,6 +1,12 @@
+"""
+Чтобы письмо отправлялось через функцию, нужно отслеживать отступы в тройных кавычках,
+используется textwrap.dedent(), чтобы эти отступы убрать, не нарушая PEP
+"""
+
 import os
 import smtplib
 from dotenv import load_dotenv
+import textwrap as tw
 
 load_dotenv()
 login = os.getenv('EMAIL_LOGIN')
@@ -8,39 +14,46 @@ password = os.getenv('EMAIL_PASSWORD')
 sender = os.getenv('SENDER_MAIL')
 receivers = os.getenv('RECEIVERS_MAIL')
 
-website = 'dvmn.org'
-link = 'https://dvmn.org/referrals/svPQW6zRN1ZjPHofEYOQbsC3YQ2HV9RyR4mpUWBb/'
-friend_name = 'Иван'
-my_name = 'Валерий'
-text = f"""
-Привет, {friend_name}! {my_name} приглашает тебя на сайт {website}!
 
-{website} — это новая версия онлайн-курса по программированию.
-Изучаем Python и не только. Решаем задачи. Получаем ревью от преподавателя.
+def send_email():
+    website = 'dvmn.org'
+    link = 'https://dvmn.org/referrals/svPQW6zRN1ZjPHofEYOQbsC3YQ2HV9RyR4mpUWBb/'
+    friend_name = 'Иван'
+    my_name = 'Валерий'
+    text = f"""
+    Привет, {friend_name}! {my_name} приглашает тебя на сайт {website}!
 
-Как будет проходить ваше обучение на {website}?
+    {website} — это новая версия онлайн-курса по программированию.
+    Изучаем Python и не только. Решаем задачи. Получаем ревью от преподавателя.
 
-→ Попрактикуешься на реальных кейсах.
-Задачи от тимлидов со стажем от 10 лет в программировании.
-→ Будешь учиться без стресса и бессонных ночей.
-Задачи не «сгорят» и не уйдут к другому. Занимайся в удобное время и ровно столько, сколько можешь.
-→ Подготовишь крепкое резюме.
-Все проекты — они же решение наших задачек — можно разместить на твоём GitHub. Работодатели такое оценят.
+    Как будет проходить ваше обучение на {website}?
 
-Регистрируйся → {link}
-На модули, которые еще не вышли, можно подписаться и получить уведомление о релизе сразу на имейл.
+    → Попрактикуешься на реальных кейсах.
+    Задачи от тимлидов со стажем от 10 лет в программировании.
+    → Будешь учиться без стресса и бессонных ночей.
+    Задачи не «сгорят» и не уйдут к другому. Занимайся в удобное время и ровно столько, сколько можешь.
+    → Подготовишь крепкое резюме.
+    Все проекты — они же решение наших задачек — можно разместить на твоём GitHub. Работодатели такое оценят.
 
-"""
-letter = f"""\
-From: {sender}
-To: {receivers}
-Subject: Курсы со скидкой 30% на dvmn.org
-Content-Type: text/plain; charset="UTF-8";
-{text}
-"""
-letter = letter.encode('utf-8')
+    Регистрируйся → {link}
+    На модули, которые еще не вышли, можно подписаться и получить уведомление о релизе сразу на имейл.
 
-server = smtplib.SMTP_SSL('smtp.yandex.ru:465')
-server.login(login, password)
-server.sendmail(sender, [receivers], letter)
-server.quit()
+    """
+    headers_and_text = f"""\
+    From: {sender}
+    To: {receivers}
+    Subject: Курсы со скидкой 30% на dvmn.org
+    Content-Type: text/plain; charset="UTF-8";
+    {text}
+    """
+    letter_without_indent = tw.dedent(headers_and_text)
+    letter = letter_without_indent.encode('utf-8')
+
+    server = smtplib.SMTP_SSL('smtp.yandex.ru:465')
+    server.login(login, password)
+    server.sendmail(sender, [receivers], letter)
+    server.quit()
+
+
+if __name__ == '__main__':
+    send_email()
